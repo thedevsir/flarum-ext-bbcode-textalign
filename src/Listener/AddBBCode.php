abd36979216a49805112e02d7e99084751580464
Amir irani <p30search@gmail.com>
2018-04-09 22:55:15 +0430

    fix Doc.

--- src/Listener/AddBBCode.php
+++ src/Listener/AddBBCode.php
@@ -24,23 +24,8 @@ class AddBBCode
     public function addBBCode(ConfigureFormatter $event)
     {
         $event->configurator->BBCodes->addCustom(
-            '[left]{DOC}[/left]',
-            '<p style="padding:0;margin:0;direction:ltr;text-align:left;">{DOC}</p>'
-        );
-
-        $event->configurator->BBCodes->addCustom(
-            '[LEFT]{DOC}[/LEFT]',
-            '<p style="padding:0;margin:0;direction:ltr;text-align:left;">{DOC}</p>'
-        );
-
-        $event->configurator->BBCodes->addCustom(
-            '[right]{DOC}[/right]',
-            '<p style="padding:0;margin:0;direction:rtl;text-align:right;">{DOC}</p>'
-        );
-
-        $event->configurator->BBCodes->addCustom(
-            '[RIGHT]{DOC}[/RIGHT]',
-            '<p style="padding:0;margin:0;direction:rtl;text-align:right;">{DOC}</p>'
+            '[LEFT]{TEXT1}[/LEFT]',
+            '<p style="padding:0;margin:0;direction:ltr;text-align:left;">{TEXT1}</p>'
         );
     }
 }
